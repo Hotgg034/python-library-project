ffba152b6770af4a098f2eae8205b182050a31fa
Hotgg034 <ghaithabbasi07@gmail.com>
2026-05-14 18:49:49 +0200

    added function to the library.py

--- library.py
+++ library.py
@@ -3,3 +3,8 @@ def borrow_book(available, title):
         return "You borrowed: " + title
     return "Not available"
 
+
+def return_book(title):
+    if not title:
+        return "Error: No title provided"
+    return "Returned: " + title
